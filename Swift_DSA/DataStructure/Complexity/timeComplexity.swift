1bdaed1d64b0a9296abf96ee96bec45b83669568
jsh9611 <jangsh9611@naver.com>
2022-05-28 15:03:53 +0900

    Removed annotation, Modify the file name.

--- Swift_DSA/DataStructure/Complexity/timeComplexity.swift
+++ Swift_DSA/DataStructure/Complexity/timeComplexity.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-//print("Hello, World!")
-
 // 양의 정수 n을 n번 더하는 계산 해보자 //
 /* Case 1 */
 func example1(n: Int) -> Int{
@@ -42,21 +40,3 @@ func example2(n: Int) -> Int {
     return sum  // 리턴 1회
 }
 print(example2(n: 10))
-
-// 공간복잡도 생각해보기
-func example3(arr: [Int], n: Int) -> Int {
-    var x = 0
-    for i in 0..<n {
-        x = x + arr[i]
-    }
-    return x
-}
-print(example3(arr: [1,2,3,4,5], n: 5))
-/*
- var arr : [Int] (4*n byte 입력 공간)
- var n : Int     (4 byte 입력 공간)
- var x : Int     (4 byte 보조 공간)
- var i : Int     (4 byte 보조 공간)
- 총 4n + 12의 메모리를 요구한다.
- 따라서 공간 복잡도는 O(n)
- */
